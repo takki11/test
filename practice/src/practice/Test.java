92eb3b15ae85ad2f60aa0696efa0b963b683c889
takizawa <takizawa@gfam.co.jp>
2026-04-10 14:24:50 +0900

    Merge branch 'main' of https://github.com/takki11/test.git into main

--- practice/src/practice/Test.java
+++ practice/src/practice/Test.java
@@ -3,6 +3,8 @@ package practice;
 public class Test {
 
     public static void main(String[] args) {
+
+        System.out.println("Hello Java World");
         System.out.println("Hello Java World");
         System.out.println("Hello Java");
         System.out.println("Hello Java");
