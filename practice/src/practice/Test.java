b8f99e654b51562eac7d89a7b75c3f699414d278
takki11 <takizawa@gfam.co.jp>
2026-04-10 14:18:59 +0900

    Update Test.java

--- practice/src/practice/Test.java
+++ practice/src/practice/Test.java
@@ -3,8 +3,6 @@ package practice;
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("Hello Java");
-        System.out.println("Hello Java");
     }
 
 }
